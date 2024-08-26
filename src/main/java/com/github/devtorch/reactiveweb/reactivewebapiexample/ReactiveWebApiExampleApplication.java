9b8d480de7cfec0ce616e93af917074dce85fc6a
Vitaliy <torchez@yandex.ru>
2024-08-26 08:31:44 +0300

    Initial commit

---
+++ src/main/java/com/github/devtorch/reactiveweb/reactivewebapiexample/ReactiveWebApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.devtorch.reactiveweb.reactivewebapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWebApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/devtorch/reactiveweb/reactivewebapiexample/ReactiveWebApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devtorch.reactiveweb.reactivewebapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
